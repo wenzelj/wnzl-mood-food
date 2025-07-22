0cbe59926dd37367a8b0c38b162aecdac5c02430
wenzelj <wenzel.joubert@gmail.com>
2025-07-22 19:39:43 +0800

    change app to be expo

--- App.js
+++
@@ -1,5 +0,0 @@
-import 'react-native-gesture-handler';
-import { registerRootComponent } from 'expo';
-import App from './src/App';
-
-registerRootComponent(App);
--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,10 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
+// https://docs.expo.dev/guides/using-eslint/
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
 
-export default tseslint.config(
-  { ignores: ["dist"] },
+module.exports = defineConfig([
+  expoConfig,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-      "@typescript-eslint/no-unused-vars": "off",
-    },
-  }
-);
+    ignores: ['dist/*'],
+  },
+]);
